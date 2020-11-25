2878866bedf02e42a7690a46f70c462330f9ad4f
jake <jacob.ray.cs@gmail.com>
2020-11-25 14:54:48 -0500

    "Updated functionality"

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-let today = new Date();
-let formatDate = today.toDateString();
-let selectElement = document.getElementById('date');
-selectElement.innerHTML = formatDate;
+// let today = new Date();
+// let formatDate = today.toDateString();
+// let selectElement = document.getElementById('date');
+// selectElement.innerHTML = formatDate;
 
 let pagesArray = ["homeBody", "projectsBody", "aboutBody","contactBody"];
 
